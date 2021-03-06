7d1018f94b24293ea64c32ab12f9b3bb86396efb
ThanosGi <33462555+ThanosGi@users.noreply.github.com>
2021-03-06 15:02:58 +0200

    Add movement in some assets

---
+++ Assets/Scripts/Movement_Random.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement_Random : MonoBehaviour
+{
+
+    NavMeshAgent navMeshAgent;
+    public float timeForNewPath = 1f;
+    bool inCoRoutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    Vector3 getNewRandomPosition()
+    {
+        float x = Random.Range(-100, 100);
+        float z = Random.Range(-100, 100);
+
+        Vector3 pos = new Vector3(x, 0, z);
+        return pos;
+    }
+
+    IEnumerator DoSomething()
+    {
+        inCoRoutine = true;
+        yield return new WaitForSeconds(timeForNewPath);
+        GetNewPath();
+        inCoRoutine = false;
+    }
+
+    void GetNewPath()
+    {
+        navMeshAgent.SetDestination(getNewRandomPosition());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inCoRoutine)
+        {
+            StartCoroutine(DoSomething());
+        }
+    }
+}
