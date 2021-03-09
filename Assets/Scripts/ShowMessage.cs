0a9ea2471f4a652ed6c238645a929d8f6aaf3b82
ChrisGiannikis <ChrisGiannikis@users.noreply.github.com>
2021-03-09 16:52:15 +0200

    Farmer pop up message

---
+++ Assets/Scripts/ShowMessage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMessage : MonoBehaviour {
+
+    public GameObject uiObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider player)
+    {
+        uiObject.SetActive(true);
+        StartCoroutine("WaitForSec");   
+    }
+
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(5);
+        uiObject.SetActive(false);
+    }
+}
